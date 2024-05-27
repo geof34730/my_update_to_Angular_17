35370b9c4183171891b4ab544e85d033ca724c2e
geoffreypetain <geoffrey.petain@gmail.com>
2024-05-27 16:19:05 +0200

    search pokemon with timer for optimize server request"

--- src/app/modules/pokemon/components/liste-pokemon/liste-pokemon.component.ts
+++ src/app/modules/pokemon/components/liste-pokemon/liste-pokemon.component.ts
@@ -6,6 +6,9 @@ import PokemonModel from "../../models/pokemon.model";
 import {Router} from "@angular/router";
 import {PokemonService} from "../../services/pokemon.service";
 import ModelPokemon from "../../models/pokemon.model";
+import {debounceTime, distinctUntilChanged, Observable, Subject, switchMap} from "rxjs";
+import pokemonData from "../../../../datas/pokemon.data";
+import * as timers from "timers";
 
 
 @Component({
@@ -24,15 +27,42 @@ import ModelPokemon from "../../models/pokemon.model";
 
 export class ListePokemonComponent implements OnInit{
   pokemonList :ModelPokemon[];
+  timerSearch :  ReturnType<typeof setTimeout>;
+  //searchTerms = new Subject<string>();
+  //pokemons$: Observable<PokemonModel[]>;
   constructor(
     private router: Router,
     private pokemonService : PokemonService
   ){}
 
   ngOnInit() {
-    this.pokemonService.getPokemonList()
-      .subscribe(pokemonList => this.pokemonList = pokemonList);
+    this.pokemonService.getPokemonList().subscribe(pokemonList => this.pokemonList = pokemonList);
   }
+
+
+
+  searchPokemon(term:string){
+    clearTimeout(this.timerSearch);
+    this.timerSearch=setTimeout(() =>{
+      this.pokemonService.searchPokemonList(term).subscribe(pokemonlist=>{
+        this.pokemonList = pokemonlist
+      });
+    },300);
+  }
+
+
+  goServiceSearchPokemon(term:string){
+    this.pokemonService.searchPokemonList(term).subscribe(pokemonList => {
+        console.log("SEARCH RESULT");
+        console.table(pokemonList);
+        this.pokemonList=pokemonList;
+      }
+    )
+
+
+
+  }
+
 
   goToPokemonFiche(pokemon:ModelPokemon){
     this.router.navigate([`/pokemons/${pokemon.id}`])
